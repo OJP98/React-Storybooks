e652acff87e137792fc1781fb679cdbf5d86f3b3
OJP98 <ojuarez_p@hotmail.com>
2019-10-24 19:02:32 -0600

    Adding custom theme

--- storybooks/.storybook/config.js
+++ storybooks/.storybook/config.js
@@ -1,4 +1,13 @@
 import { configure } from '@storybook/react';
+import { addParameters } from '@storybook/react';
+import { themes } from '@storybook/theming';
+import customTheme from './customTheme';
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
+
+addParameters({
+  options: {
+    theme: customTheme,
+  },
+});
