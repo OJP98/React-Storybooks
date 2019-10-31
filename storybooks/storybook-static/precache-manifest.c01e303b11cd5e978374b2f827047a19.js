a18d2713ca61ca4482143fbd5de2b2517596454e
OJP98 <ojuarez_p@hotmail.com>
2019-10-31 01:17:02 -0600

    Final storybook build

---
+++ storybooks/storybook-static/precache-manifest.c01e303b11cd5e978374b2f827047a19.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f33fa560155f1ee21278b54386180ee",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.95d57f4a9c9263ceac64.bundle.js"
+  },
+  {
+    "revision": "95b22f939710768e03a5",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.95d57f4a9c9263ceac64.bundle.js"
+  },
+  {
+    "url": "vendors~main.95d57f4a9c9263ceac64.bundle.js"
+  }
+]);
